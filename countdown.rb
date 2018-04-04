4bc28dfd94dbe05357927fc1d5d3259b2f185480
Mary Jack <maryjackinthebox@gmail.com>
2018-04-04 02:19:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(x)
   end
   return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(x)
+  x = 10
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
